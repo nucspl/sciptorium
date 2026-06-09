46e26904fee5ac60c5cdbd57119bddda88445408
nucspliki <80261260+nucspl@users.noreply.github.com>
2026-06-10 04:04:49 +0800

    Update gdl_cyclic.py
    Conventional revisions: Standalone return on "-> None"; Implementation revisions: stop signal cascade, TakeWhile loops, DateTime simplification, device sleep-wake proofing, non-shadowing names in `stage_events()`, removed custom spinners.
    
    This is as clean as I'll go.

--- python/gdl/cyclic/gdl_cyclic.py
+++ python/gdl/cyclic/gdl_cyclic.py
@@ -1,99 +1,56 @@
 import signal
 import subprocess
-from collections import Counter
+from collections import Counter, defaultdict as DefaultDict
+from collections.abc import Iterator
 from contextlib import suppress as Suppress
-from datetime import (
-    datetime as DateTime,
-    timedelta as TimeDelta,
-    timezone as TimeZone,
-)
+from datetime import datetime as DateTime, timedelta as TimeDelta
 from functools import partial as Partial
+from itertools import chain as Chain, takewhile as TakeWhile
 from subprocess import Popen
 from threading import Event, Thread
-from typing import Any, Iterator
+from typing import Any
 
 from fire import Fire
-from rich import live
 from rich.console import Console
-from rich.spinner import SPINNERS, Spinner
+from rich.live import Live
+from rich.spinner import Spinner
 
-# -- preparation
+
+# -- prep
 
 
-SPINNERS.update({
-    'stream': {
-        'interval': 100,
-        'frames': [
-            '   ',
-            '   ',
-            '   ',
-            '·  ',
-            '*· ',
-            '**·',
-            '·**',
-            ' ·*',
-            '  ·',
-        ],
-    },
-    'pulse': {
-        'interval': 100,
-        'frames': [
-            '   ',
-            '   ',
-            '   ',
-            ' · ',
-            ' * ',
-            ' · ',
-        ],
-    },
-    'cycle': {
-        'interval': 100,
-        'frames': [
-            ' ⠧ ',
-            ' ⠏ ',
-            ' ⠛ ',
-            ' ⠹ ',
-            ' ⠼ ',
-            ' ⠶ ',
-        ],
-    },
-})
-
-console = Console(
-    color_system=None,
-    highlight=False,
-)
-Live = Partial(
-    live.Live,
-    refresh_per_second=60,
-    transient=True,
-    console=console,
-)
+console = Console(color_system=None, highlight=False)
+Live = Partial(Live, refresh_per_second=60, transient=True, console=console)
 
 
 # -- stop
 
 
-def watch_signal() -> tuple[Event, Event]:
-    signal_event, stop_event = Event(), Event()
+def stage_events() -> tuple[Event, Event]:
+    _signal_event, _stop_event = Event(), Event()
 
-    def notify_signal() -> None:
-        signal_event.wait()
-        with Live(Spinner('cycle', 'Stopping')):
-            stop_event.wait()
+    def notify_stop() -> None:
+        _signal_event.wait()
+        with Live(Spinner('dots', 'Stopping')):
+            _stop_event.wait()
+
+    def cascade_stop() -> None:
+        _stop_event.wait()
+        handle_signal()
 
     def handle_signal(*_: Any) -> None:
-        return signal_event.set()
+        _signal_event.set()
 
     for s in signal.valid_signals():
         with Suppress(OSError, ValueError):
             signal.signal(s, handle_signal)
 
-    Thread(target=notify_signal).start()
-    return signal_event, stop_event
+    Thread(target=notify_stop).start()
+    Thread(target=cascade_stop).start()
+    return _signal_event, _stop_event
 
 
-signal_event, stop_event = watch_signal()
+signal_event, stop_event = stage_events()
 
 
 # -- main
@@ -101,74 +58,72 @@ signal_event, stop_event = watch_signal()
 
 def spawn(link: str) -> Iterator[str]:
     with Popen(
-        ('gallery-dl', link),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-    ) as popen:
+      ('gallery-dl', link),
+      stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+      text=True) as popen:
         if not popen.stdout or not popen.stderr:
-            return popen.terminate()
+            popen.terminate()
+            return
 
-        for _ in popen.stdout:
-            if signal_event.is_set():
-                break
-            yield _.strip()
-
-        for _ in popen.stderr:
-            if signal_event.is_set():
-                break
+        for _ in TakeWhile(
+          lambda _: not signal_event.is_set(),
+          Chain(popen.stdout, popen.stderr)):
             yield _.strip()
 
 
-def cycle(*links: str) -> DateTime:
-    processed: list[str] = []
+def cycle(*links: str, time: DateTime) -> None:
+    items: dict[str, list[str]] = DefaultDict(list)
+    with Live(spinner := Spinner('dots')):
+        for a, b in TakeWhile(
+          lambda _: not signal_event.is_set(),
+          enumerate(links)):
+            spinner.update(text=f'[{a + 1}/{len(links)}] {b}')
+            for c in spawn(b):
+                if (d := c[0]) in ('.', '#', '['):
+                    items[d].append(c)
+                if d != '#':
+                    console.print(c)
 
-    with Live(spinner := Spinner('stream')) as live:
-        for a, b in enumerate(links):
-            if signal_event.is_set():
-                break
-            spinner.update(text=f'[{a + 1}/{len(links)}]: {b}')
-            for _ in spawn(b):
-                if not _.startswith('#'):
-                    live.console.print(_)
-                if not _.startswith('['):
-                    processed.append(_)
+    downloads, skips, errors = (
+      len(items['.']), len(items['#']), len(items['[']))
 
-    downloaded, skipped, time = (
-        sum(1 for _ in processed if not _.startswith(('#', '['))),
-        sum(1 for _ in processed if _.startswith(('#', '['))),
-        DateTime.now(TimeZone.utc).astimezone(),
-    )
+    if not downloads and not skips and errors:
+        console.print('! All provided links failed.')
+        stop_event.set()
+        return
 
     console.print(
-        f'[{time:%Y-%m-%dT%H:%M:%SZ}] '
-        f'{len(processed)} items processed: '
-        f'{downloaded} downloaded; '
-        f'{skipped} skipped.',
-        sep='\n',
-    )
-
-    return time
+      f'[{time.astimezone():%Y-%m-%dT%H:%M:%SZ}] {downloads + skips} items '
+      f'processed: {downloads} downloaded; {skips} skipped.',
+      sep='\n')
 
 
-def main(*links: str, interval: int = 3600) -> None:
+def main(*links: str, interval: int = 1800) -> None:
     """
     Routinely runs gallery-dl while omitting unnecessary console printing.
     Args:
-        interval: How many seconds to wait for after each gallery-dl run.
+        interval: How many seconds to wait with each gallery-dl run.
     """
-    if any(_ > 1 for _ in Counter(links).values()):
-        links = tuple(dict.fromkeys(links))
-        console.print('* Duplicates found and sanitised.')
-    console.print(*links, sep='\n', end='\n' * 2)
+    if not links:
+        console.print('! No links were provided.')
+        stop_event.set()
+        return
+
+    counter = Counter(links)
+    if any(_ > 1 for _ in counter.values()):
+        links = tuple(counter.keys())
+        console.print('* Duplicates found, sanitised links:')
+        console.print(*links, sep='\n', end='\n' * 2)
 
     while not signal_event.is_set():
-        time = cycle(*links) + TimeDelta(seconds=interval)
-        with Live(spinner := Spinner('pulse')):
-            spinner.update(text=f'Waiting until {time:%Y-%m-%dT%H:%M:%SZ}.')
-            signal_event.wait(interval)
+        cycle(*links, time=(time := DateTime.now()))
+        time += TimeDelta(seconds=interval)
+        with Live(Spinner('dots', f'Waiting until {time:%H:%M:%S}')):
+            while not signal_event.is_set() and time > DateTime.now():
+                signal_event.wait(1)
 
-    return stop_event.set()
+    stop_event.set()
 
 
 if __name__ == '__main__':
